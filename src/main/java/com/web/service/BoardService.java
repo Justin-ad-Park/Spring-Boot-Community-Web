573e035d7fb57fb6ba1bdebeaf987450eee5d9fb
Justin Park <justin@Justinui-Mac-Pro.local>
2018-11-13 19:44:00 +0900

    FIRST COMMIT

---
+++ src/main/java/com/web/service/BoardService.java
@@ -0,0 +1,36 @@
+package com.web.service;
+
+import com.web.domain.Board;
+import com.web.repository.BoardRepository;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by KimYJ on 2017-07-13.
+ */
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @Cacheable(value="cache10", key="#pageable")
+    public Page<Board> findBoardList(Pageable pageable) {
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
+
+        System.out.println("******************Page Number : " + pageable.getPageNumber());
+
+        return boardRepository.findAll(pageable);
+    }
+
+    public Board findBoardByIdx(Long idx) {
+        return boardRepository.findById(idx).orElse(new Board());
+    }
+
+}
